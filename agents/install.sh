a8f2d804d42e48b3a202810aba6de6aa5f28296c
JohnGUnderwood <4124456+JohnGUnderwood@users.noreply.github.com>
2024-03-18 20:47:20 +0000

    Add timeout for agents

--- agents/install.sh
+++ agents/install.sh
@@ -14,7 +14,7 @@ if ! command -v python3 &> /dev/null && ! command -v pip3 &> /dev/null; then
 fi
 
 echo "Install finished. Running the agent API at localhost:3010 with flask and gunicorn"
-. venv/bin/activate && gunicorn app:app -b 0.0.0.0:3010
+. venv/bin/activate && gunicorn app:app -b 0.0.0.0:3010 --timeout 120
 
 echo 
 echo "============================="
--- agents/run.sh
+++ agents/run.sh
@@ -1,2 +1,2 @@
 echo "Running the agent API at localhost:3010 with flask and gunicorn"
-. venv/bin/activate && gunicorn app:app -b 0.0.0.0:3010
+. venv/bin/activate && gunicorn app:app -b 0.0.0.0:3010 --timeout 120
